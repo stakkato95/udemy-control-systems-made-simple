4fa78f080da6f86d3e2c73243f77838ce00bbe92
Artsiom Kaliaha <stakkato95@gmail.com>
2022-07-16 08:25:10 +0200

    Section 3. Add battery with charge and discharge resistance

--- 3/1_battery/params_setup.m
+++ 3/1_battery/params_setup.m
@@ -1,3 +1,10 @@
-I = 2.3;
 Cn = 2.3 * 3600; % battery capacity, Amp/Hour
 Sim_Time = 3600; % simulation runs for one hour
+
+% % charging from 0%
+% I = 2.3;
+% initialCurrent = 0;
+
+% discharging from 100%
+I = -2.3;
+initialCurrent = Cn;
